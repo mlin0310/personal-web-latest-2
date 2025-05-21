6878d9d5710885c7c74181ab84ab8091625f8bad
Morris Lim <user@MorrisdeMacBook-Pro.local>
2025-05-21 18:21:28 +0800

    Fix unescaped entities in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block">Hi, I'm</span>
+              <span className="block">Hi, I&apos;m</span>
               <span className="block text-yellow-500">Morris Lin</span>
             </h1>
             <div className="mt-8 flex justify-center">
